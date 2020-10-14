46eab2b33467dd2b825977606755c381efb62781
SanderKeulers <55051955+SanderKeulers@users.noreply.github.com>
2020-10-14 12:55:09 +0200

    Update MAIOproject_1.py

--- MAIOproject_1.py
+++ MAIOproject_1.py
@@ -26,46 +26,41 @@ for key in Data.keys():
     for key in RollingStd.keys():
         RollingStd[key]  = Data[key].rolling(window=3,win_type='boxcar').std()
 
-threshold_lon = 0.0001
-threshold_lat = 0.00001
+threshold_lon = 0.000005
+threshold_lat = 0.0000005
 
 for key in RollingStd.keys():
-    for i in range(RollingStd[key].index[0],(len(RollingStd[key]['lon'])-1)):
+    for i in range(RollingStd[key].index[0],(RollingStd[key].index[0]+len(RollingStd[key]['lon'])-1)):
         if np.abs(RollingStd[key]['lon'][i+1] - RollingStd[key]['lon'][i]) > threshold_lon:
            Data[key].loc[i+1] = np.nan
-        if np.abs(RollingStd[key]['lon'][i+1] - RollingStd[key]['lon'][i]) == np.nan:
-           Data[key].loc[i+1] = np.nan 
+        if RollingStd[key]['lon'][i] == np.nan:
+           Data[key].loc[i] = np.nan 
         if np.abs(RollingStd[key]['lat'][i+1] - RollingStd[key]['lat'][i]) > threshold_lat:
            Data[key].loc[i+1] = np.nan 
-        if np.abs(RollingStd[key]['lat'][i+1] - RollingStd[key]['lat'][i]) == np.nan:
-           Data[key].loc[i+1] = np.nan    
+        if RollingStd[key]['lat'][i] == np.nan:
+           Data[key].loc[i] = np.nan    
             
 for key in Data.keys():
     for key in RollingMean.keys():
         RollingMean[key] = Data[key].rolling(window=24,win_type='boxcar',min_periods = 10).mean()
 
 
-for key in Data.keys():
-    count = Data[key]['lon'].isna().sum()
-    
-
 
 
 for key in Data.keys():
-    plt.scatter(Data[key]['lon'][:-1],Data[key]['lat'][:-1])
+    plt.scatter(Data[key]['lon'][:-1],Data[key]['lat'][:-1]) # Laatste punt is bijna altijd slecht omdat RollingStd wordt berekend tot eind-1 dus in plot ook alles tot eind-1
     plt.xlabel('Degrees longitude')
     plt.ylabel('Degrees latitude')
-    plt.title('K-Transect #4')
-    
-for key in RollingMean.keys():
-    plt.scatter(RollingMean[key]['lon'][:-1],RollingMean[key]['lat'][:-1])
-    plt.xlabel('Degrees longitude')
-    plt.ylabel('Degrees latitude')
-    plt.title('K-Transect #4')
+    plt.title('K-Transect #4 '+'threshold_lat='+str(threshold_lat)+', threshold_lon='+str(threshold_lon))
      
 
 plt.show()
 
-# Er ligt hier nog 1 behoorlijke outlier maar dat komt doordat de Data rond het window van 24 1 NaN heeft (omdat er een GPS punt buiten de initiele bounds ligt) 
-# Hierdoor krijgt het hele window van RollingStd 23 punten met NaNs waardoor de ene outlier er niet uit wordt gehaald want NaN[t+1] - NaN[t] 
+#### Displacements
+
+#Displacement  = {elem : pd.DataFrame for elem in NrJumps}
+#for key in RollingMean.keys():
+ #   for i in range(RollingMean[key].index[0],(RollingMean[key].index[0]+len(RollingMean[key]['lon'])-1)):
+  #      Displacement[key] = RollingMean[key]['lat'] [i+1] - RollingMean[key]['lat'] [i]
+   #     Displacement[key] = RollingMean[key]['lon'] [i+1] - RollingMean[key]['lon'] [i]
 
