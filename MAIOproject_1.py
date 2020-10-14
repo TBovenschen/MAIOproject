2226c4f66a2764c945eb62447a8426479cc5831a
SanderKeulers <55051955+SanderKeulers@users.noreply.github.com>
2020-10-14 14:02:47 +0200

    Update MAIOproject_1.py

--- MAIOproject_1.py
+++ MAIOproject_1.py
@@ -26,8 +26,8 @@ for key in Data.keys():
     for key in RollingStd.keys():
         RollingStd[key]  = Data[key].rolling(window=3,win_type='boxcar').std()
 
-threshold_lon = 0.000005
-threshold_lat = 0.0000005
+threshold_lon = 0.00001
+threshold_lat = 0.00001
 
 for key in RollingStd.keys():
     for i in range(RollingStd[key].index[0],(RollingStd[key].index[0]+len(RollingStd[key]['lon'])-1)):
@@ -48,19 +48,18 @@ for key in Data.keys():
 
 
 for key in Data.keys():
-    plt.scatter(Data[key]['lon'][:-1],Data[key]['lat'][:-1]) # Laatste punt is bijna altijd slecht omdat RollingStd wordt berekend tot eind-1 dus in plot ook alles tot eind-1
+    plt.scatter(Data[key]['lon'][:-1],Data[key]['lat'][:-1],s=4) # Laatste punt is bijna altijd slecht omdat RollingStd wordt berekend tot eind-1 dus in plot ook alles tot eind-1
     plt.xlabel('Degrees longitude')
     plt.ylabel('Degrees latitude')
     plt.title('K-Transect #4 '+'threshold_lat='+str(threshold_lat)+', threshold_lon='+str(threshold_lon))
-     
+    plt.ticklabel_format(useOffset=False, style='plain')
 
 plt.show()
 
 #### Displacements
-
-#Displacement  = {elem : pd.DataFrame for elem in NrJumps}
-#for key in RollingMean.keys():
- #   for i in range(RollingMean[key].index[0],(RollingMean[key].index[0]+len(RollingMean[key]['lon'])-1)):
-  #      Displacement[key] = RollingMean[key]['lat'] [i+1] - RollingMean[key]['lat'] [i]
-   #     Displacement[key] = RollingMean[key]['lon'] [i+1] - RollingMean[key]['lon'] [i]
+#Displacement = []
+for key in RollingMean.keys():
+    for i in range(RollingMean[key].index[0],(RollingMean[key].index[0]+len(RollingMean[key]['lon'])-1)):
+        Displacement[key] = RollingMean[key]['lat'] [i+1] - RollingMean[key]['lat'] [i]
+        Displacement[key] = RollingMean[key]['lon'] [i+1] - RollingMean[key]['lon'] [i]
 
