b7875b745d4bcea838509148b533229a8da842bb
TBovenschen <71378919+TBovenschen@users.noreply.github.com>
2020-10-14 14:22:57 +0200

    update

--- MAIOproject.py
+++ MAIOproject.py
@@ -12,30 +12,48 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file = open(r'Ktransect-S7_input.txt')
-
+#Read  the data from a file
+file = 'Ktransect-S4_input.txt'
 df = pd.read_csv(file, comment='#', delim_whitespace=True)
 
+#Store data in numpy arrays
 time = np.asarray(df['jday'])
 lat = np.asarray(df['lat'])
 lon = np.asarray(df['lon'])
 
-
-latMasked = np.ma.masked_outside(lat,64,71)
-lonMasked = np.ma.masked_outside(lon,-50,-49)
+#Mask values that are wrongly measured
+latMasked = np.ma.masked_outside(lat,67,69)
+lonMasked = np.ma.masked_outside(lon,-50.5,-50.1)
 latMasked.filled(np.nan)
 lonMasked.filled(np.nan)
-
-latmean = np.nanmean(latMasked)
-lonmean = np.nanmean(lonMasked)
+    
 
 
+#Range to calculate the moving average
 N = 100
-result1 = np.convolve(latMasked, np.ones((N,))/N, mode='valid')
+#Calculate moving averages
+lat_mean = np.ma.convolve(latMasked, np.ones((N,))/N, mode='valid')
+lon_mean= np.ma.convolve(lonMasked, np.ones((N,))/N, mode='valid')
 
-latMasked = np.ma.masked_outside(latMasked,np.nanmean(latMasked)-np.nanstd(latMasked),np.nanmean(latMasked)+np.nanstd(latMasked))
-lonMasked = np.ma.masked_outside(lonMasked,np.nanmean(lonMasked)-np.nanstd(lonMasked),np.nanmean(lonMasked)+np.nanstd(lonMasked))
+latMasked = latMasked[int(N/2):int(-N/2+1)]
+lonMasked = lonMasked[int(N/2):int(-N/2+1)]
+    
+latMasked1 = np.ma.masked_where((latMasked < lat_mean-np.ma.std(latMasked)) & 
+                                (latMasked > lat_mean+np.ma.std(latMasked)), latMasked)
 
+# latMasked1 = np.ma.filled(latMasked1, np.nan)
+# latMasked2 = latMasked1[~np.isnan(latMasked1)]
+# latMasked1.compressed()
+# lonMasked = np.ma.masked_where(lonMasked,lon_mean-np.nanstd(lonMasked),lon_mean+np.nanstd(lonMasked))
+#     if (latMasked[i] < lat_mean[i]-np.nanstd(latMasked)) or (latMasked[i] > lat_mean[i]+np.nanstd(latMasked)):
+#         latMasked[i] = np.nan
+# latMasked = np.ma.masked_invalid(latMasked)
+        
 plt.figure()
-plt.scatter(lonMasked,latMasked)
+plt.scatter(lonMasked,latMasked1)
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
 plt.show()
+
+
+
